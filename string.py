1e410b364f76237c0677389a148d2dea459f2f20
SKYHMT <96362398+SKYHMT@users.noreply.github.com>
2021-12-20 22:26:12 +0800

    Create string.py

---
+++ string.py
@@ -0,0 +1,19 @@
+
+#     01234                 -1
+# item="Mandy is learning python"
+# print(item[-1])
+# print(item[0:5])
+# # 5 is index, if don't use the index, will show all the strings, if we don't use the start, start assume be 0
+# print(item[0:])
+# print(item[:])
+# formatted strings
+# first = "Mandy"
+# last = "HE"
+# message=first + ' [' +last+ '] is a student'
+# msg = f'{first} [{last}] is a student'
+# print(msg)
+# item = "Mandy is a student"
+# print(len(item)) this is called function
+# print(item.upper())  this is called method
+# item = "Mandy is a student"
+# print("Mandy" in item) (in is boolean function, if we have or not)
