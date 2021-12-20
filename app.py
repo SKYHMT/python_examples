4c047e00f12851c24c0ce4816e713846635b2870
SKYHMT <96362398+SKYHMT@users.noreply.github.com>
2021-12-20 22:24:24 +0800

    update

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-print("Mandy HE")
-# draw shape
-print("*" * 8)
+# print("Mandy HE")
+# # draw shape
+# print("*" * 8)
+# age=20
+# print(age)
+
+name="John Smith"
+print(name)
 age=20
 print(age)
+new_patient=True
+print(new_patient)
--- input.py
+++ input.py
@@ -1,2 +1,24 @@
-name=input("what is your name? ")
-print("Hello "+name)
+# name=input("what is your name? ")
+# print("Hello "+name)
+# name=input("what is your name? ")
+# color=input("What is your favorite color? ")
+# print( name  + " likes " + color )
+# value of input is always a string, need to be convert to an integer or float.
+# int(convert to an integer ), float(convert to a float), bool(boolean)
+# birth_year = input("Birth year: ")
+# age = 2021- int(birth_year)
+# print(age)
+# weigh_in_pounds=input("what is your weight? ")
+# weight_in_kilo=int(weigh_in_pounds)/2.2046
+# print(weight_in_kilo)
+# always need single quotes or double quotes for a string
+# course='python for "beginners"'
+# print(course)
+# for multiple length, we use the triple quotes
+# email='''Hi Darren,
+# This is Mandy from HKUST
+# Nice to get touch with you
+# Thank you
+# Mandy
+# '''
+# print(email)
