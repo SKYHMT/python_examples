9eb19b0998779c0f93732a372fcc578f566c9b03
SKYHMT <96362398+SKYHMT@users.noreply.github.com>
2022-01-03 11:55:08 +0800

    import package & modules

---
+++ modules.py
@@ -0,0 +1,16 @@
+# import conventors
+# print(conventors.lbs_to_kg(70))
+#
+# from conventors import lbs_to_kg
+# print(lbs_to_kg(70))
+
+# two ways to import a function from another program
+
+# exercise: find_max
+
+from program_pool import find_max
+numbers = [2, 5, 6, 7, 12, 45, 1, 34]
+max = find_max(numbers)
+print(max)
+
+
